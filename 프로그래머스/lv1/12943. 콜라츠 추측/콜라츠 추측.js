0ec7d624c18b04cb3006fd9bf999158c8302ac75
Jinsu Park <116702892+jinsupark4255@users.noreply.github.com>
2023-01-26 23:28:35 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,18 @@
+function solution(num) {
+    var answer = 0;
+    if(num===1)
+        return 0;
+    while(num!==1){
+        if(answer>=500)
+            return -1;
+        if(num%2===0){
+            num/=2;
+            answer++;
+        }
+        else{
+            num=num*3+1;
+            answer++;
+        }
+    }
+    return answer;
+}
