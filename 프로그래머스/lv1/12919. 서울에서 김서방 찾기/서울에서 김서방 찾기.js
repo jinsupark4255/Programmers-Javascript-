455807f3f640afcb75e7cde0061ec635ee3020ec
Jinsu Park <116702892+jinsupark4255@users.noreply.github.com>
2023-01-26 23:28:44 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,10 @@
+function solution(seoul) {
+    var answer = '';
+    let tmp;
+    for(let i =0;i<seoul.length;i++){
+        if(seoul[i]=='Kim'){
+            tmp = i;
+        }
+    }
+    return `김서방은 ${tmp}에 있다`;
+}
