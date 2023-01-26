6e9d8d8e4c3fbaf7a68772fe90e8f09270e7296b
Jinsu Park <116702892+jinsupark4255@users.noreply.github.com>
2023-01-26 23:32:54 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.46 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,15 @@
+function solution(numbers) {
+    var answer = [];
+    
+    for(let i =0;i<numbers.length;i++){
+        for(let j = i+1;j<numbers.length;j++){
+            answer.push(numbers[i]+numbers[j]);
+        }
+    }
+    answer=[...new Set(answer)];
+    answer=answer.sort(function(a,b){
+        return a-b;
+    })
+    
+    return answer;
+}
