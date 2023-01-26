e086931e5b9f631865ece4faf56e693ef5c807ec
Jinsu Park <116702892+jinsupark4255@users.noreply.github.com>
2023-01-26 23:54:04 +0900

    [level 1] Title: 소수 찾기, Time: 23.38 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,20 +1,22 @@
 function solution(n) {
     let answer = 0;
-    const arr = new Array(n+1).fill(true);
+    const arr = new Array(n+1).fill(1);
+    console.log(arr);
       
     for(let i = 2; i <= n; ++i){
         // 이미 소수가 아닌 인덱스는 건너뛴다.
-        if(arr[i] === false){
+        if(arr[i] === 0){
             continue; 
         }
         // 배수는 소수가 아니라 0으로 설정
         for(let k = i * 2; k <= n; k += i){
-            arr[k] = false;
+            arr[k] = 0;
         }
     }
     // 소수의 갯수를 구한다.
+    console.log(arr);
     for(let i = 2; i <= n; ++i){
-        if(arr[i] === true){
+        if(arr[i] === 1){
             answer++;
         }
     }
