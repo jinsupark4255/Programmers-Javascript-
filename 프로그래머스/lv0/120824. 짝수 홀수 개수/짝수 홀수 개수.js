47c587588747f8865455fcf5076a9e34197be9e9
Jinsu Park <116702892+jinsupark4255@users.noreply.github.com>
2023-01-26 23:12:06 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,16 @@
+function solution(num_list) {
+    var answer = [];
+    let a=0;
+    let b=0;
+    for(let i =0;i<num_list.length;i++){
+        if(num_list[i]%2===0){
+            a++;
+        }
+        else{
+            b++;
+        }
+    }
+    answer[0] = a;
+    answer[1] = b;
+    return answer;
+}
