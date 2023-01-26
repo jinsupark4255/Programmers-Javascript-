5809fff8f92cb3188042d8e74147ef73852c5cc0
Jinsu Park <116702892+jinsupark4255@users.noreply.github.com>
2023-01-26 23:18:08 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+    var answer = 0;
+    let max = numbers[0]*numbers[1];
+    for(let i =0;i<numbers.length;i++){
+        for(let j=i+1;j<numbers.length;j++){
+            if(max<numbers[i]*numbers[j]){
+                max = numbers[i]*numbers[j];
+            }
+        }
+    }
+    answer = max;
+    return answer;
+}
