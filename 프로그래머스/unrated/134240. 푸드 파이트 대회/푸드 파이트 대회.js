ddeff058fc48b4d40f5452c14c25fbc1d0dde58a
Jinsu Park <116702892+jinsupark4255@users.noreply.github.com>
2023-01-27 21:20:58 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,13 @@
+function solution(food) {
+    var answer = '';
+    for(let i =1;i<food.length;i++){
+        for(let j =0;j<Math.floor(food[i]/2);j++){
+            answer+=i;
+        }
+    }
+    let rev = answer.split('').reverse();
+    answer+='0';
+    answer+=rev.join('');
+    
+    return answer;
+}
