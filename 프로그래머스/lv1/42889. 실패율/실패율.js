cd80115c0904cfdb6a53ac9517d6e8bc57aa7ffc
Jinsu Park <116702892+jinsupark4255@users.noreply.github.com>
2023-01-30 23:52:08 +0900

    [level 1] Title: 실패율, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -0,0 +1,28 @@
+function solution(N, stages) {
+    var answer = [];
+    
+    stages.sort((a, b) => a - b);
+    
+    let obj = {};
+    for (let i = 1; i <= N; i++) {
+        obj[i] = 0;
+    }
+    
+    for (let i in stages) {
+        for (let j = 1; j <= stages[i]; j++) {
+            obj[j] += 1;
+        }
+    }
+    
+    let tmp = [];
+    
+    for (let i = 0; i < N; i++) {
+        let fail = (stages.filter(item => item === (i + 1)).length) / obj[i + 1];
+        tmp.push([fail, i + 1]);
+    }
+    
+    tmp.sort((a, b) => b[0] - a[0]);
+    tmp.map(a => answer.push(a[1]));
+    
+    return answer;
+}
