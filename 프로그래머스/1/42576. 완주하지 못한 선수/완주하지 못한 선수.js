32ddd15bb9c32380e50e0c0fc6ed919953450f45
Jinsu Park <jin04007@naver.com>
2024-03-20 11:55:25 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 37.30 ms, Memory: 66.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,21 +1,21 @@
 function solution(participant, completion) {
-    let hashMap = new Map();
-
-    // completion 배열의 요소를 맵에 추가 (키는 요소, 값은 0으로 시작)
-    for (let i = 0; i < completion.length; i++) {
-        hashMap.set(completion[i], (hashMap.get(completion[i]) || 0) + 1);
+    const myMap = new Map();
+    for(let x of participant){
+        if(!myMap.get(x)){
+            myMap.set(x,1);
+        }
+        else{
+            myMap.set(x,myMap.get(x)+1);
+        }
     }
-
-    // participant 배열을 순회하며 맵에서 요소를 찾고, 카운트를 감소
-    for (let i = 0; i < participant.length; i++) {
-        if (hashMap.has(participant[i])) {
-            hashMap.set(participant[i], hashMap.get(participant[i]) - 1);
-            if (hashMap.get(participant[i]) === 0) {
-                hashMap.delete(participant[i]);
-            }
-        } else {
-            // 완주하지 못한 참가자를 찾음
-            return participant[i];
+    for(let x of completion){
+        if(myMap.has(x)){
+            myMap.set(x,myMap.get(x)-1);
+        }
+    }
+    for(let x of participant){
+        if(myMap.get(x)>=1){
+            return x;
         }
     }
 }
