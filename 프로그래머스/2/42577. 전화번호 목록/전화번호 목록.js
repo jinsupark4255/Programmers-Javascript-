6c5e4a955077e5d52308d4f3cd35564dcf56ba0c
Jinsu Park <jin04007@naver.com>
2024-03-20 12:29:56 +0900

    [level 2] Title: 전화번호 목록, Time: 97.08 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,12 +1,9 @@
 function solution(phone_book) {
-    var answer = true;
-    phone_book.sort();
-    console.log(phone_book);
+    let arr = phone_book.sort();
     for(let i =0;i<phone_book.length-1;i++){
         if(phone_book[i]==phone_book[i+1].substr(0,phone_book[i].length)){
             return false;
         }
-        
     }
     return true;
 }
