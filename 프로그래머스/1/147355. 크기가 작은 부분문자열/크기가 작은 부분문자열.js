6558286a2c45479b520034d90e4d07f1de47bcc1
Jinsu Park <jin04007@naver.com>
2024-06-19 14:03:07 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 2.78 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,19 +1,11 @@
 function solution(t, p) {
-    let arr = [];
-    let answer = 0;
-    for(let i =0;i<t.length;i++){
-        let sum = 0;
-        for(let j =i;j<i+p.length;j++){
-            sum+=Number(t[j])
-            sum*=10;
-        }
-        arr.push(sum/10);
-    }
-    console.log(arr)
-    for(let i =0;i<arr.length;i++){
-        if(arr[i]<=Number(p)){
-            answer++;
+    let sum = 0;
+    for(let i =0;i<=t.length-p.length;i++){
+        let value = t.slice(i,i+p.length);
+        console.log(value)
+        if(parseInt(value)<=parseInt(p)){
+            sum++;
         }
     }
-    return answer;
+    return sum;
 }
