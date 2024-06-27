1d204b4eb2afd5a65240de0bfe1dc3ffec721847
Jinsu Park <jin04007@naver.com>
2024-06-27 16:01:01 +0900

    [level 2] Title: 배달, Time: 0.41 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -1,40 +1,31 @@
 function solution(N, road, K) {
-    const graph = new Array(N+1).fill(0).map(v => [])
-    
-    for (const [s,e,d] of road){
-        graph[s].push([e,d])
-        graph[e].push([s,d])
+  const graph = new Array(N + 1);
+for (let i = 0; i < graph.length; i++) {
+    graph[i] = [];
+}
+
+
+  let distance = Array.from({ length: N + 1 }, () => Infinity);
+
+  let queue = [];
+  for (let [a, b, c] of road) {
+    graph[a].push([b, c]);
+    graph[b].push([a, c]);
+  }
+ 
+  queue.push([1, 0]);
+  distance[1] = 0;
+  while (queue.length) {
+    const [point, cost] = queue.shift();
+    for (let i = 0; i < graph[point].length; i++) {
+      const next = graph[point][i][0];
+      const nextcost = graph[point][i][1];
+      if (distance[next] > distance[point] + nextcost) {
+        distance[next] = distance[point] + nextcost;
+        queue.push([next, nextcost]);
+      }
     }
-    
-    const dp = new Array(N+1).fill(Infinity)
-    const visited = new Array(N+1).fill(false)
-    
-    dp[1] = 0
-    
-    const findSmallNode = () => {
-        let ret = 1
-        let tmp = Infinity
-    
-        for (let i = 1 ; i < dp.length ; i++){
-            if (!visited[i] && dp[i] < tmp) {
-                tmp = dp[i]
-                ret = i
-            }
-        }
-        return ret
-    }
-    
-    
-    for (let i = 1 ; i < dp.length ; i++) {
-        const node = findSmallNode()
-        visited[node] = true
-        
-        for (const [e,d] of graph[node]) {
-            if (!visited[e] && dp[node]+d < dp[e]){
-                dp[e] = dp[node]+d
-            }
-        }
-    }
-    
-    return dp.filter(v=> v <= K).length
+  }
+  distance = distance.filter((v) => v <= K);
+  return distance.length;
 }
