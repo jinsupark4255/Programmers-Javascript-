2bb155d88a77373423b08f78f4fc7e92ce0f0003
박진수 (Jinsu Park) <jin04007@naver.com>
2024-07-12 11:48:14 +0900

    [level 2] Title: 전화번호 목록, Time: 96.90 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577#) 
 
 ### 성능 요약
 
-메모리: 76.6 MB, 시간: 97.08 ms
+메모리: 75.4 MB, 시간: 96.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 20일 12:29:55
+2024년 07월 12일 11:48:12
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,7 +1,11 @@
 function solution(phone_book) {
-    let arr = phone_book.sort();
-    for(let i =0;i<phone_book.length-1;i++){
-        if(phone_book[i]==phone_book[i+1].substr(0,phone_book[i].length)){
+    // 전화번호부를 사전순으로 정렬합니다.
+    phone_book.sort();
+    
+    // 정렬된 전화번호부에서 각 번호를 바로 다음 번호와 비교합니다.
+    for (let i = 0; i < phone_book.length - 1; i++) {
+        // 현재 번호가 다음 번호의 접두사인지 확인합니다.
+        if (phone_book[i + 1].startsWith(phone_book[i])) {
             return false;
         }
     }
