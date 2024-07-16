54e7874d42e808b70e1479525f8723d68e7fc3ad
박진수 (Jinsu Park) <jin04007@naver.com>
2024-07-16 09:29:09 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.31 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let arr = new Array(n).fill(0);
+    arr[0] = 1;
+    arr[1] = 1;
+    for(let i =2;i<=n;i++){
+        arr[i] = (arr[i-1]+arr[i-2])%1234567
+    }
+    return arr[n]
+}
