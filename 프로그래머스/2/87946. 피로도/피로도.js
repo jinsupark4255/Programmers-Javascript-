945c1b7fb625cce908e438c1d9be39915c0dea7a
박진수 (Jinsu Park) <jin04007@naver.com>
2024-07-25 17:39:22 +0900

    [level 2] Title: 피로도, Time: 6.19 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,21 +1,17 @@
 function solution(k, dungeons) {
-  let answer = [];
-  let visited = Array(dungeons.length).fill(0);
-
-  function dfs(count, k) {
-    answer.push(count);
-
-    for (let i = 0; i < dungeons.length; i++) {
-      let current = dungeons[i];
-      if (k >= current[0] && !visited[i]) {
-        visited[i] = 1;
-        dfs(count + 1, k - current[1]);
-        visited[i] = 0;
-      }
+    let arr = new Array(dungeons.length).fill(0);
+    let answer = [];
+    
+    function dfs(k,count){
+        answer.push(count);
+        for(let i =0;i<dungeons.length;i++){
+            if(k>=dungeons[i][0]&&!arr[i]){
+                arr[i] = 1;
+                dfs(k-dungeons[i][1],count+1);
+                arr[i] = 0;
+            }
+        }
     }
-  }
-
-  dfs(0, k);
-
-  return Math.max(...answer);
+    dfs(k,0)
+    return Math.max(...answer)
 }
