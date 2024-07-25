ca44a4196daf576179acb53cc2f75ba5b6d32c63
박진수 (Jinsu Park) <jin04007@naver.com>
2024-07-25 15:52:28 +0900

    [level 2] Title: 타겟 넘버, Time: 17.12 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,17 +1,15 @@
 function solution(numbers, target) {
-    let depth = 0;
     let answer = 0;
-    const sum = (dep,acc) =>{
-        if(dep<numbers.length){
-            sum(dep+1,acc+numbers[dep]);
-            sum(dep+1,acc-numbers[dep]);
-        }
-        else{
-            if(acc==target){
+    dfs(0,0);
+    function dfs(index,sum){
+        if(index===numbers.length){
+            if(sum===target){
                 answer++;
             }
+            return;
         }
+        dfs(index+1,sum-numbers[index]);
+        dfs(index+1,sum+numbers[index]);
     }
-    sum(0,0);
     return answer;
 }
