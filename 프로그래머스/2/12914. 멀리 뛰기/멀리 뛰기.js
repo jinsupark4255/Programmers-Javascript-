249acf6cca94275609d2b415e699754e03ab639a
박진수 (Jinsu Park) <jin04007@naver.com>
2024-08-01 09:30:15 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.50 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,9 +1,12 @@
 function solution(n) {
-    let arr = new Array(n).fill(0);
-    arr[0] = 1;
-    arr[1] = 1;
-    for(let i =2;i<=n;i++){
-        arr[i] = (arr[i-1]+arr[i-2])%1234567
+    // 동적계획법을 이용한 피보나치
+    const dp = Array.from({length:n}).fill(0); // n길이의 dp배열을 생성
+    dp[0] = 1, dp[1] = 1; // 0번과 1번에 1 대입
+    
+  	// 피보나치 풀이, 다만 dp배열에 넣을 때, %1234567을 해줘야 에러가 나지 않는다.
+  	for (let i=2; i<=n; i++) {
+        dp[i] = (dp[i-2] + dp[i-1])%1234567;
     }
-    return arr[n]
+    
+    return dp[n];
 }
