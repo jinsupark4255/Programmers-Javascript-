172cb6ed96cbd833eb5b7a1e160472ec73bf10ed
박진수 (Jinsu Park) <jin04007@naver.com>
2024-08-27 16:02:58 +0900

    [level 2] Title: 괄호 회전하기, Time: 143.82 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,28 +1,30 @@
 function solution(s) {
-    let oriarr = []
-    for(let i =0;i<s.length;i++){
-        oriarr.push(s[i]);
-    }
-    let len = s.length;
-    let tmp = 0;
+    let total = s.length;
+    let array = s.split('')
     let sum = 0;
-    while(tmp<len-1){
+    while(total!=0){
         let arr = [];
-        arr.push(oriarr[0]);
-        for(let i =1;i<oriarr.length;i++){
-            if((arr[arr.length-1]==='('&& oriarr[i] === ')') || (arr[arr.length-1]==='{'&& oriarr[i] === '}')||(arr[arr.length-1]==='['&& oriarr[i] === ']')){
+        let tmp = array.shift();
+        arr.push(tmp);
+        for(let i =0;i<array.length;i++){
+            if(arr[arr.length-1]==='['&&array[i]===']'){
                 arr.pop();
-            }else{
-                arr.push(oriarr[i]);
+            }
+            else if(arr[arr.length-1]==='('&&array[i]===')'){
+                arr.pop();
+            }
+            else if(arr[arr.length-1]==='{'&&array[i]==='}'){
+                arr.pop();
+            }
+            else{
+                arr.push(array[i]);
             }
         }
         if(arr.length===0){
             sum++;
         }
-        tmp++;
-        let tmp2 = oriarr.shift();
-        oriarr.push(tmp2);
+        array.push(tmp);
+        total--;
     }
     return sum;
-    
 }
